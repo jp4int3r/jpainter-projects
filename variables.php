c4715806f30873d7fd23a61ddd75c8f01d71bc7e
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-02-07 15:21:05 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,32 @@
+<doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php //Script 2.3 - variables.php
+
+// name
+$FirstName = "Jeffrey";
+$LastName = "Painter";
+
+// address
+$street = "6428 Ray Rd";
+$city = "Phoenix";
+$state = "AZ";
+$zip = "85044";
+
+// phone number
+$phone = "(555)345-9572";
+
+
+// print info
+
+print "<p> My name is $FirstName $LastName <br> 
+I live at <br> $street <br> $city $state $zip <br>
+My phone number is $phone</p>";
+
+?>
+</body>
+</html>
