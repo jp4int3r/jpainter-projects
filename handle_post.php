d17ceb0c8255e21db96dd0052bb04fe4699c7259
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-02-29 23:32:53 -0700

    Add files via upload

---
+++ handle_post.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Forum Posting</title>
+</head>
+<body>
+<?php
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$posting = $_POST['posting'];
+
+$name = $first_name . ' ' . $last_name;
+
+print "<div>Thank you, $name, for your posting:
+<p>$posting</p></div>";
+
+?>
+</body>
+</html>
