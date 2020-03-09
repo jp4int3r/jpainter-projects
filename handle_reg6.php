c27a9bc729037ed019042487375b74fbbd945963
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-03-08 21:34:35 -0700

    Add files via upload

---
+++ handle_reg6.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<style type="text/css"
+	media="screen">
+		.error { color: red; }
+	</style>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php
+
+ini_set('display_errors', 1);
+
+
+$okay = true;
+
+if (empty($_POST['email'])) {
+	print '<p class="error">Please enter your email address.</p>';
+	$okay = false;
+}
+if (empty($_POST['password'])) {
+	print '<p class="error">Please enter your password.</p>';
+	$okay = false;
+}
+
+if ($_POST['password'] != $_POST['confirm']) {
+	print '<p class="error"> Your confirmed password does not match
+	the original password.</p>';
+	$okay = false;
+}
+
+if (is_numeric($_POST['year']) AND 
+(strlen($_POST['year']) ==4) ) {
+
+	if ($_POST['year'] < 2020) {
+		$age = 2020 - $_POST['year'];
+	} else {
+		print'<p class= "error">Either you entered your birth year wrong
+	or you come from the future!</p>';
+		$okay = false;
+	}
+} else { 
+	print'<p class= "error">Please enter the year your were born 
+	as four digits.</p>';
+	$okay = false;
+}
+if (!isset($_POST['terms'])) {
+	print '<p class="error">You must accept the terms.</p>';
+	$okay = false;
+}
+
+if ($_POST['color'] == 'red'){
+	$color_type = 'primary';
+} elseif ($_POST['color'] == 'yellow') {
+	$color_type = 'primary';
+} elseif ($_POST['color'] == 'green') {
+	$color_type = 'secondary';
+} elseif ($_POST['color'] == 'blue') {
+	$color_type = 'primary';
+} elseif ($_POST['color'] == 'black') {
+	$color_type = 'secondary';
+} elseif ($_POST['color'] == 'orange') {
+	$color_type = 'secondary';
+} else {
+	print '<p class="error">Please select your favorite color.</p>';
+	$okay = false;
+}
+
+
+
+
+if ($okay) {
+	print '<p>You have been successfully registered (but not really).</p>';
+	print "<p>You will turn $age this year.</p>";
+	print "<p>Your favorite color is a $color_type color.</p>";
+}
+?>
+</body>
+</html>
