f3086e639d94f51dcea3dd03ea5de12429cc7013
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-03-09 11:40:56 -0700

    Add files via upload

---
+++ soups1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>No Soup for You!</title>
+	</style>
+</head>
+<body>
+<h1>Mmm...soups</h1>
+<?php
+
+$soups = [
+		'Monday' => 'Clam Chowder',
+		'Tuesday' => 'White Chicken Chili',
+		'Wednesday' => 'Vegetarian',
+		'Thursday' => 'Meatball',
+		'Friday' => 'Chicken Noodle',
+		'Saturday' => 'Beef Stew',
+		'Sunday' => 'Minestrone',
+		];
+		
+print "<p>$soups</p>";
+
+print_r($soups);
+
+?>
+</body>
+</html>
