da5f8be8e416d3601b45ca0e7fe25f3567431c8b
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-03-08 20:29:30 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php
+
+ini_set('display_errors', 1);
+
+
+$okay = true;
+
+if ($okay) {
+	print '<p>You have been successfully registered (but not really).</p>';
+}
+?>
+</body>
+</html>
