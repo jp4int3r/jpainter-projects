02884412f5649e1513582d38604c70577af7c1e5
jp4int3r <60458522+jp4int3r@users.noreply.github.com>
2020-03-22 16:50:29 -0700

    Add files via upload

---
+++ books.php
@@ -0,0 +1,20 @@
+<?php //Script 8.5
+
+define('TITLE', 'Books by J.D. Salinger');
+include('header.html');
+?>
+
+<h2>J.D. Salinger's Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+
+<?php include('footer.html');
+?>
+
+	
+	
+	
